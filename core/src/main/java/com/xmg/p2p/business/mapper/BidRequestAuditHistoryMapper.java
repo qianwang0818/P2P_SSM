d0203b750fdf5b2701e703f071f926fbff360270
Li Ran <646663536@qq.com>
2018-02-28 20:11:48 +0800

    day08_04 后台借款审核. 完成

---
+++ core/src/main/java/com/xmg/p2p/business/mapper/BidRequestAuditHistoryMapper.java
@@ -0,0 +1,15 @@
+package com.xmg.p2p.business.mapper;
+
+import com.xmg.p2p.business.domain.BidRequestAuditHistory;
+import java.util.List;
+
+/**
+ * day08_03
+ */
+public interface BidRequestAuditHistoryMapper {
+
+    int insert(BidRequestAuditHistory record);
+
+    BidRequestAuditHistory selectByPrimaryKey(Long id);
+
+}
